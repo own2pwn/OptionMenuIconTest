c591ee05de4c1ca3a6b57905eec65fa401a3d5d0
Manuel Meyer <manuel@vikingosegundo.de>
2015-08-30 18:26:28 +0200

    How to feature images for menu options that adept to different cell heights.

--- OptionMenuIconTest/ViewController.swift
+++ OptionMenuIconTest/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UITableViewController {
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
     }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject]? {
 
+        let moreClosure = { (action: UITableViewRowAction!, indexPath: NSIndexPath!) -> Void in
+            println("More closure called")
+        }
+        
+        let moreAction = UITableViewRowAction(style: .Normal, title: "  ", handler: moreClosure)
+        
+        if let image = UIImage(named: "star.png"){
+            moreAction.backgroundColor = UIColor(patternImage: image)
 
+        }
+        return [moreAction]
+    }
+    
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        // Intentionally blank. Required to use UITableViewRowActions
+    }
+    
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+            return  min((44.0 * CGFloat(indexPath.row) + 1), 122.0)
+    }
 }
 
---
+++ README.md
@@ -0,0 +1,5 @@
+answering stack overflow question [Custom table view row action (image)][1]
+
+How to feature images for menu options that adept to different cell heights. 
+
+[1]: http://stackoverflow.com/questions/32206302/custom-table-view-row-action-image
